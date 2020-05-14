1db73890499fff0b62fce7cf8af76e80e3f0f999
Everton Rodrigues Resende <ecoresende@hotmail.com>
2020-05-14 09:42:08 -0300

    creation of a list adapter

--- app/src/main/java/com/everton/mononuclealanticoorps/MainActivity.kt
+++ app/src/main/java/com/everton/mononuclealanticoorps/MainActivity.kt
@@ -1,16 +1,13 @@
 package com.everton.mononuclealanticoorps
 
+import android.content.Context
 import android.content.Intent
 import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase.openDatabase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
-import android.view.View
-import android.widget.SearchView
-import android.widget.SimpleCursorAdapter
-import android.widget.Toolbar
+import android.view.*
+import android.widget.*
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main.view.*
 import java.sql.SQLException
@@ -21,6 +18,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val listView = findViewById<ListView>(R.id.main_ListView)
+        listView.adapter = MyCustomAdapter(this)
 
         var search = search_view
 
@@ -39,7 +39,46 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    private class MyCustomAdapter(context: Context) : BaseAdapter() {
 
+        private val mContext: Context
+
+        private val names = arrayListOf<String>(
+            "Dipirona", "Valium", "opioide", "dorflex"
+        )
+        init {
+            mContext = context
+        }
+
+        override fun getView(position: Int, convertView: View?, ViewGroup: ViewGroup?): View {
+            val layoutInflater = LayoutInflater.from(mContext)
+            val rowMain = layoutInflater.inflate(R.layout.main_row, ViewGroup, false)
+
+            val nameTextView = rowMain.findViewById<TextView>(R.id.monoclonal_TextView)
+            nameTextView.text = names.get(position)
+
+            return rowMain
+
+
+        }
+
+        override fun getItem(position: Int): Any {
+            return "TEXT STRING"
+
+        }
+
+        override fun getItemId(position: Int): Long {
+            return position.toLong()
+
+        }
+
+        override fun getCount(): Int {
+            return names.size
+
+
+        }
+
+    }
 
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
